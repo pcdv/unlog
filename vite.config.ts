4108419ad21cdd7720a08d54b239035c11fc7b92
pcdv <pcdv.github@gmail.com>
2026-05-05 21:34:40 +0200

    Fix dev build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { readFileSync } from 'fs'
-
-const pkg = JSON.parse(readFileSync('./package.json', 'utf-8')) as { version: string }
+import pkg from './package.json'
 
 export default defineConfig({
   plugins: [react()],
